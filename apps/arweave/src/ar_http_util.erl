99cbd164a9bd285e191532369586722790f964c1
zhongzichang <zhongzichang@gmail.com>
2026-05-06 01:20:34 +0800

    fix: merge bug

--- apps/arweave/src/ar_http_util.erl
+++ apps/arweave/src/ar_http_util.erl
@@ -3,6 +3,7 @@
 -export([get_tx_content_type/1, arweave_peer/1]).
 
 -include_lib("arweave/include/ar.hrl").
+-include_lib("arweave_config/include/arweave_config.hrl").
 -include_lib("eunit/include/eunit.hrl").
 
 -define(PRINTABLE_ASCII_REGEX, "^[ -~]*$").
